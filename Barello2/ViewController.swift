55b8b89cf34b047f95d090eed42359694fff6eec
Sergej Lotz <blondewalker@gmail.com>
2017-07-01 13:19:22 +0200

    Add files

--- Barello2/ViewController.swift
+++ Barello2/ViewController.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var backgroundMusicPlayer = AVAudioPlayer()
+    
+    func playBackgroundMusic(filename: String) {
+        let url = Bundle.main.path(forResource: filename, ofType: "mp3")
+        do {
+            //if backgroundMusicPlayer.isPlaying {
+              //  backgroundMusicPlayer.pause()
+            //}
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: url! ))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            backgroundMusicPlayer.prepareToPlay()
+            backgroundMusicPlayer.currentTime = 0
+            backgroundMusicPlayer.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    //this runs the do try statement
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func play1(_ sender: AnyObject) {
+        playBackgroundMusic(filename: "kassa")
+    }
+    @IBAction func play2(_ sender: AnyObject) {
+        playBackgroundMusic(filename: "police")
+    }
+    //@IBAction func stop(_ sender: AnyObject) {
+    //    audioPlayer.stop()
+    //}
+    
 
 
 }
